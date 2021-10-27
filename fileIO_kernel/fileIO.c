8fb16212bbadc911b4f066ef14f785a6e52a2507
Guo <hugo2135@gmail.com>
2021-10-27 04:03:54 -0700

    最後修改

--- fileIO_kernel/fileIO.c
+++ fileIO_kernel/fileIO.c
@@ -6,79 +6,34 @@
 #include <asm/segment.h>
 #include <asm/uaccess.h>
 
-MODULE_DESCRIPTION("file_IO_try");
+MODULE_DESCRIPTION("fileIO");
 MODULE_LICENSE("GPL");
 
-mm_segment_t oldfs;
-
-struct file *openfile(char *path,int flag,int mode) 
-{ 
-    struct file *fp; 
-    
-    fp=filp_open(path, flag, mode); 
-    if (fp){
-        printk("File is opened");
-        return fp;
-    }
-    else{
-        printk("File not open");
-        return NULL;
-    } ; 
-}
-
-int file_pos_write(struct file *fp, loff_t *pos, ssize_t rt){
-    struct kiocb kiocb;
-    init_sync_kiocb(&kiocb, fp);
-    kiocb.ki_pos = pos ? *pos : 0;
-    printk("pos : %ld",&pos);
-    if(rt>0)
-        if (pos){
-            *pos = kiocb.ki_pos;
-            return 0;
-        }
-    return -1;
-}
- 
-int closefile(struct file *fp) 
-{ 
-    filp_close(fp,NULL); 
-    return 0; 
-} 
+#define LEN 260
 
 static int fileIO_init(void){
-    int readlen = 512;
-    char __user buf[readlen];
+    char buf[LEN];
     struct  file *fp;
     struct  file *fp_out;
-    ssize_t rt;
     int ret;
+    
     printk("--------------");
     printk(KERN_INFO "File accessing\n");
-    fp = openfile("input.txt", O_RDONLY, 0);
-    fp_out = openfile("output.txt", O_WRONLY, 0);
+    fp = filp_open("input.txt", O_RDONLY, 0);
+    fp_out = filp_open("output.txt", O_CREAT|O_RDWR, S_IROTH);
+
     if(fp != NULL){
-        memset(buf, 0, readlen);
-        ret = kernel_read(fp, buf, readlen, fp->f_pos);
-        char a;
-        // char tmp[ret];
-        int count = 0;
-        // memset(tmp, 0, ret);
-        
-        printk("f_pos:%lld", fp->f_pos);
-        printk("buf:%s", buf);
-        printk("buf addr 1:%p", buf[0]);
-        printk("buf addr 2:%p", buf[1]);
-        
-        printk("ret:%d", ret);
-        while(count<=ret){
-            a = buf[ret-count];
-            rt = kernel_write(fp_out, &a, 1, fp_out->f_pos);
-            file_pos_write(fp_out, fp_out->f_pos, rt);
+        memset(buf, 0, LEN);
+        ret = kernel_read(fp, buf, LEN, &(fp->f_pos));
+        int count = 1;
+
+        while(count <= LEN){
+            kernel_write(fp_out, buf + (LEN - count), 1, &(fp_out->f_pos));
             count++;
         }
-        // printk("tmp:%s", tmp);
-        closefile(fp);
-        closefile(fp_out);
+
+        filp_close(fp, NULL);
+        filp_close(fp_out, NULL);
     }
 
     return 0;
